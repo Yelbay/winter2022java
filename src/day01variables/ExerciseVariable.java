4baffc5418d2e9d10af2c3aea94b421da56bf7d6
“Yelbaygit config --global user.email cnntylby@gmail.comgit config --global credential.username Yelbaygit config --global credential.password Cennet1983 <cnntylby@gmail.comgit config --global user.name Yelbay>
2022-05-05 12:14:06 -0400

    day01

---
+++ src/day01variables/ExerciseVariable.java
@@ -0,0 +1,18 @@
+package day01variables;
+
+public class ExerciseVariable {
+    public static void main(String[] args) {
+       boolean isExpensive= true;
+       boolean isCold=false;
+        System.out.println(isExpensive);
+        System.out.println(isCold);
+        int number=14;
+
+        System.out.println(number);
+        byte age=73;
+        System.out.println(age);
+
+
+
+    }
+}
